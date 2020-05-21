4417cd85606670ef6b5be1fc28597fc7d2ddd6e3
Yuqi Zhong <damon.yq.zhong@gmail.com>
2020-05-20 23:04:59 -0400

    Add criteria section styling and validation functions

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//display selected password length
+var slider = document.getElementById("charNum");
+var output = document.getElementById("passwordLength");
+output.innerHTML = slider.value;
+slider.oninput = function() {
+output.innerHTML = this.value;
+}
+
+//Check if character type(s) selected
+function CheckBox() {
+  if(document.getElementById("lowercase").checked == true ||
+  document.getElementById("uppercase").checked == true ||
+  document.getElementById("numeric").checked == true ||
+  document.getElementById("specialChar").checked == true
+  ){
+          document.getElementById("criteria").style.display = "none";
+      } else {
+          alert(`None of character types is selected, please select at least one.`);
+      }
+  }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,3 +33,9 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//
+function generatePassword(){
+  var 
+}
+
